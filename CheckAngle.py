d20c0e61fc934150b4f04632531ef3b78ebe75b0
jingsam <jingsam@163.com>
2015-12-31 18:00:00 +0800

    check angle

--- CheckAngle.py
+++ CheckAngle.py
@@ -2,22 +2,7 @@
 __author__ = 'jingsam@163.com'
 
 import os
-from math import pi
 import arcpy
-
-
-def calc_angle(point1, point2, point3, sr):
-    a = (point2.X - point1.X, point2.Y - point1.Y)
-    b = (point3.X - point2.X, point3.Y - point2.Y)
-
-    ab = a[0] * b[0] + a[1] * b[1]
-    al = arcpy.Polyline([[point1.X, point1.Y], [point2.X, point2.Y]], sr)
-    bl = arcpy.Polyline([[point2.X, point2.Y], [point3.X, point3.Y]], sr)
-    a_b = ((a[0] ** 2 + a[1] ** 2) ** 0.5) * ((b[0] ** 2 + b[1] ** 2) ** 0.5)
-    if a_b == 0:
-        return 180
-
-    return 180 - abs(ab) / a_b * 180 / pi
 
 
 def check_angle(in_fc, tolerance, out_chk):
@@ -38,20 +23,29 @@ def check_angle(in_fc, tolerance, out_chk):
     if ext != '.csv':
         out_chk += '.csv'
     f = open(out_chk, 'w')
-    f.write('OID, ErrorID, X, Y\n')
+    f.write('OID, ErrorID, X, Y, distance\n')
 
+    errors = []
     cursor = arcpy.da.SearchCursor(in_fc, ["SHAPE@", "OID@"], spatial_reference=desc.spatialReference.GCS)
     for row in cursor:
         for part in row[0]:
-            for i in xrange(0, len(part) - 1):
-                p1, p2, p3 = i - 1, i, i + 1
-                if i == 0:
-                    p1 = len(part) - 2
+            for i in xrange(0, len(part) - 4):
+                p1, p2, p3, p4 = i, i + 1, i + 2, i + 3
 
-                angle = calc_angle(part[p1], part[p2], part[p3], desc.spatialReference.GCS)
-                if angle <= tolerance:
-                    f.write('{0}, {1}, {2}, {3}, {4}\n'.format(row[1], 'ERR06', part[p2].X, part[p2].Y, angle))
+                point1 = arcpy.PointGeometry(p1, desc.spatialReference.GCS)
+                point3 = arcpy.PointGeometry(p3, desc.spatialReference.GCS)
+                point4 = arcpy.PointGeometry(p4, desc.spatialReference.GCS)
+
+                p1_p3 = point1.angleAndDistanceTo(point3)[1]
+                p1_p4 = point1.angleAndDistanceTo(point4)[1]
+
+                dist = min(p1_p3, p1_p4)
+
+                if dist <= tolerance:
+                    errors.append('{0}, {1}, {2}, {3}, {4}\n'.format(row[1], 'ERR06', part[p2].X, part[p2].Y, dist))
     del cursor
+
+    f.write(''.join(errors))
     f.close()
 
 
